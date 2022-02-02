79379a348868c1ee4f08004d0b9fb91b46cea2e2
Kane Jackson <kane@kanejackson.com>
2022-02-03 04:25:46 +1100

    adding back the mongo config with new password

---
+++ mongoConfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+    uri: "mongodb://admin:dajFItP6uG06oYbF@cluster0-shard-00-00.fv9cb.mongodb.net:27017,cluster0-shard-00-01.fv9cb.mongodb.net:27017,cluster0-shard-00-02.fv9cb.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mmezjd-shard-0&authSource=admin&retryWrites=true&w=majority"
+}
